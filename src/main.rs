23afaaa00904e6d445d964b5b201c66babd6c056
lijing00333 <lijing00333@163.com>
2025-08-24 20:20:23 +0800

    add timeout async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 use rdev::{listen, Event, EventType};
+use tokio;
+use tokio::time::{sleep, Duration};
+
+async fn set_timeout<F>(f: F, delay_ms: u64)
+where
+    F: FnOnce() + Send + 'static,
+{
+    tokio::spawn(async move {
+        sleep(Duration::from_millis(delay_ms)).await;
+        f();
+    });
+}
 
 // bindgen 可生成更完整的绑定，这里手动声明
 unsafe extern "C" {
@@ -19,7 +31,6 @@ fn get_selected_text() -> String {
             let rust_str = std::ffi::CStr::from_ptr(c_str).to_string_lossy().into_owned();
             free_selected_text_from_oc(c_str);
             return rust_str;
-            // println!("✅ 选中文本: {}", rust_str);
 
             // 可选：释放内存（如果需要）
             // free_selected_text_from_oc(c_str);
@@ -29,7 +40,8 @@ fn get_selected_text() -> String {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("================ main begin!==================");
     // unsafe {
     //     let hello_text = get_hello_from_oc();
@@ -43,12 +55,15 @@ fn main() {
         match event.event_type {
             EventType::KeyPress(key) => {
                 println!("⌨️ KeyPress: {:?}", key);
-            }
+            },
             EventType::ButtonRelease(button) => {
-                let tmp_text: String = get_selected_text();
                 println!("🖱️ ButtonRelease: {:?}", button);
-                println!(">>>: {:?}", tmp_text);
-            }
+                tokio::spawn(async {
+                    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+                    let tmp_text: String = get_selected_text();
+                    println!(">>>: {:?}", tmp_text);
+                });
+            },
             _ => {}
         }
     }).expect("无法启动监听");
